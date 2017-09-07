57ea554a7e40d740242ec6fbdb3ddc8ed0c57130
谭林 <joker@tanlindeMacBook-Pro.local>
2017-09-07 18:01:11 +0800

    add variable and func

---
+++ func.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Date: 2017/9/7
+ * @author joker <tanlin@staff.weibo.com>
+ */
---
+++ variable.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Date: 2017/9/7
+ * @author joker <tanlin@staff.weibo.com>
+ */
+/**
+ * false condition
+ * integer 0
+ * double 0.0
+ * string ""
+ * string "0"
+ * boolean false
+ * array array()/[]
+ * null NULL
+ */
+
+/**
+ * array_key_exists and isset
+ * $arr = array("key" => null)
+ * array_key_exists('key', $arr) => true
+ * isset($arr["key"]) => false
+ */
+
+/**
+ * change $a and $b
+ * list($a, $b) = array($b, $a)
+ */
+
+/**
+ * var_export print original php code
+ * $info = array("a" => 1, "b" => 2)
+ * var_export($info) => array("a" => 1, "b" => 2)
+ */
+
+
